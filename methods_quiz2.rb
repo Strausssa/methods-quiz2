54ef152f7967c4359d5711b298ffb4b2014fd885
Sam Strauss <strausssa@s.dcsdk12.org>
2015-09-11 10:13:17 -0600

    methods_quiz2 complete

--- methods_quiz2.rb
+++ methods_quiz2.rb
@@ -1,5 +1,55 @@
-module MethodsQuiz
-	
-	
+module MethodsQuiz2
+  
+  def without_doubles(dice2, dice3, no_doubles)
+    if dice2 == dice3 && no_doubles
+      dice2 + dice3 + 1
+    else
+      dice2 + dice3
+    end
+  end
+
+  def max_maybe(num1, num2)
+    if num1 == num2
+      return 0
+    elsif num2 % 5 == num1 % 5
+      if num1 > num2
+        num2
+      else
+        num1
+      end
+    elsif num1 > num2
+      num1
+    elsif num2 > num1
+      num2
+    end
+  end
+
+  def squirrels_play?(temper, summer)
+    if summer
+      if temper <= 100 && temper >= 60
+        return true
+      else
+        return false
+      end
+    else
+      if temper <= 90 && temper >= 60
+        return true
+      else
+        return false
+      end
+    end
+  end
+
+  def red_ticket(a, b, c)
+    if a == 2 && b == 2 && c == 2
+      return 10
+    elsif (a == 1 && b == 1 && c == 1) || (a == 0 && b == 0 && c == 0)
+      return 5
+    elsif a != b && a != c
+      return 1
+    else
+      return 0
+    end
+  end
 
 end
--- methods_quiz2_test.rb
+++ methods_quiz2_test.rb
@@ -11,6 +11,35 @@ class MethodsQuizTest < MiniTest::Test
    	end.new
 	end
 
-	# TODO - write tests here
+	def test_without_doubles
+		assert_equal 11, @m.without_doubles(5, 6, false)
+		assert_equal 8, @m.without_doubles(5, 3, true)
+		assert_equal 7, @m.without_doubles(3, 3, true)
+		assert_equal 6, @m.without_doubles(3, 3, false)
+		assert_equal 13, @m.without_doubles(5, 8, false)
+		assert_equal 8, @m.without_doubles(5, 3, true)
+	end
 
+	def test_max_maybe
+		assert_equal 3, @m.max_maybe(2, 3)
+		assert_equal 18, @m.max_maybe(7, 18)
+		assert_equal 0, @m.max_maybe(5, 5)
+		assert_equal 6, @m.max_maybe(16, 6)
+		assert_equal 24, @m.max_maybe(24, 6)
+	end
+
+	def test_squirrels_play?
+		assert_equal false, @m.squirrels_play?(7, true)
+		assert_equal true, @m.squirrels_play?(67, false)
+		assert_equal true, @m.squirrels_play?(100, true)
+		assert_equal false, @m.squirrels_play?(96, false)
+		assert_equal false, @m.squirrels_play?(94, false)
+	end
+
+	def test_red_ticket
+		assert_equal 1, @m.red_ticket(2, 0, 1)
+		assert_equal 0, @m.red_ticket(1, 1, 2)
+		assert_equal 10, @m.red_ticket(2, 2, 2)
+		assert_equal 5, @m.red_ticket(1, 1, 1)
+	end
 end
